f138143d9f1385ef7fcd5936a61b8dd6cb32467b
brucealberto <brucealberto2@gmail.com>
2022-08-14 12:47:35 -0300

    feat: Home page

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,5 +1,33 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import styles from './styles.module.css'
 export function Home () {
+  const url = "https://jsonplaceholder.typicode.com/posts"
+  // const [xablau, setXablau] = useState([])
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect( ()=> {
+    async function getPosts() {
+      const { data } = await axios.get(url)
+      setPosts(data)
+    } 
+    getPosts();
+    // setXablau(xab)
+  }, [])
+
+
   return (
-    <h1>Tela Home</h1>
+    <div className={styles.container}>
+      <h2>Listagem</h2>
+      <div className={styles.posts}>
+        {posts.map((item) => (
+        <div className={styles.post} key={item.id}>
+          <h4>{item.id} - {item.title}</h4>
+          <p>{item.body}</p>
+        </div>
+         ))}
+      </div>
+    </div>
   )
 }
